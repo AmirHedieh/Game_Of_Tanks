9963264f22357c1ad655c64e66c9f36fb6787473
Amirhossein Hediehloo <amirhosseinhediehloo@gmail.com>
2018-06-10 01:24:37 +0430

    Tank - Gun - Turret - Bullet class created. Tank Class initialized.

---
+++ src/game/elements/Gun.java
@@ -0,0 +1,10 @@
+package game.elements;
+
+/**
+ * a weapon that can provide damage to other objects.
+ * two kind of guns are available: 1- Missle launcher 2- Machine gun
+ *
+ */
+public class Gun {
+
+}
---
+++ src/game/elements/Tank.java
@@ -0,0 +1,71 @@
+package game.elements;
+
+/**
+ * an entity which is a moving object though the map.
+ * it contains a gun to hit other objects. it also has
+ * specific amount of health. if health comes down to zero
+ * the tank becomes dead.
+ * type determines that if the tank is AI or Player(1 for AI, 0 for Player).
+ */
+public class Tank {
+    //fields
+    private int health;
+    private Gun gun;
+    private int type;
+
+    //constrcutor
+    public Tank(int health, Gun gun, int type){
+        this.health = health;
+        this.gun = gun;
+        this.type = type;
+    }
+
+    //methods
+
+    /**
+     * set amount of health for tank.
+     * @param health
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    /**
+     * set a gun for tank.
+     * @param gun
+     */
+    public void setGun(Gun gun) {
+        this.gun = gun;
+    }
+
+    /**
+     * set a type for tank(1 for AI, 0 for Player).
+     * @param type
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * @return amount of health remained for tank
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * @return gun of the tank.
+     */
+    public Gun getGun() {
+        return gun;
+    }
+
+    /**
+     * 0 if tank is AI.
+     * 1 if tank is Player.
+     * @return the type of tank
+     */
+    public int getType() {
+        return type;
+    }
+}
