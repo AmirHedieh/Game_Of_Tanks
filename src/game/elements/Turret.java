daa2d63635987c76ba3b0c3b6459df2eb2838d8d
Amirhossein Hediehloo <amirhosseinhediehloo@gmail.com>
2018-06-10 01:56:09 +0430

    Turret Class initialized.

--- src/game/elements/Turret.java
+++ src/game/elements/Turret.java
@@ -1,5 +1,15 @@
 package game.elements;
 
+/**
+ * a Turret is a unmovable kind of weapon stuck to the ground.
+ * turrets have infinite amount of ammo.
+ * turrets also has health, when it gets down to zero means it is destroyed.
+ */
 public class Turret {
+    //fields
 
+    //constructor
+
+    //methods
+    
 }
