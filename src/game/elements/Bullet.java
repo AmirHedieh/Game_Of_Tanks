25e1de7a8fe37a5e623023b08f5a1bbb07d58a39
Amirhossein Hediehloo <amirhosseinhediehloo@gmail.com>
2018-06-10 01:51:57 +0430

    Bullet Class initialized.

--- src/game/elements/Bullet.java
+++ src/game/elements/Bullet.java
@@ -1,5 +1,29 @@
 package game.elements;
 
-public class Bullet {
+/**
+ * each Gun makes bullets to hit others.
+ * Bullets takes their damages from the gun which made them.
+ */
+public class Bullet { // todo: 2 classes must extend bullet ( missle - machine gun)
+    //fields
+    private int damage;
+    //constructor
 
+    //methods
+
+    /**
+     * set damage for bullet
+     * @param damage amount of damage it must provides
+     */
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    /**
+     * get damage of bullet
+     * @return amount if damage it provides
+     */
+    public int getDamage() {
+        return damage;
+    }
 }
