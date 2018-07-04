03ff4f95fc10a7ca9868c5dc72b2b646d755709c
Ali Nazari <ali.nazari@aut.ac.ir>
2018-07-04 11:47:53 +0430

    Remove the now ignored directory node_modules

--- src/game/map/SpriteSheet.java
+++ src/game/map/SpriteSheet.java
@@ -14,7 +14,7 @@ public class SpriteSheet
 
     public SpriteSheet(String path)
     {
-        BufferedImage image = Utility.loadImage("");
+        BufferedImage image = Utility.loadImage("src/resource/sprite_sheet.png");
 
         this.path = path;
         this.width = image.getWidth();
@@ -22,5 +22,14 @@ public class SpriteSheet
 
         pixels = image.getRGB(0, 0, width, height, null, 0, width);
 
+        for (int i =0; i < pixels.length; i++)
+        {
+            pixels[i] = (pixels[i] & 0xff) / 64;
+        }
+
+        for (int i =0; i < 8; i++)
+        {
+            System.out.println(pixels[i]);
+        }
     }
 }
